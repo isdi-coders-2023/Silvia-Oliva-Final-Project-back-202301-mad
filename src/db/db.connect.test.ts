1921651f32d4f4c6d35a5035b60b9d7ed2ee6989
Sivlia <silvolivm@gmail.com>
2023-03-14 21:49:58 +0100

    delete db.connect test

--- src/db/db.connect.test.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-import { config } from '../config.js';
-
-const { user, passwd, cluster, dbName } = config;
-
-export const dbConnect = () => {
-  const uri = `mongodb+srv://${user}:${passwd}@${cluster}/${dbName}?retryWrites=true&w=majority`;
-  return mongoose.connect(uri);
-};
