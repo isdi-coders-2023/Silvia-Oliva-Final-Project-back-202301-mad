e60cc75ad23990845e9aef73d65d3566d387beb1
Sivlia <silvolivm@gmail.com>
2023-03-14 18:49:13 +0100

    add db.connect

--- src/db/db.connect.ts
+++ src/db/db.connect.ts
@@ -1,9 +1,15 @@
 import mongoose from 'mongoose';
 import { config } from '../config.js';
+import createDebug from 'debug';
+
+const debug = createDebug('PF:DBconnect');
 
 const { user, passwd, cluster, dbName } = config;
 
-export const dbConnect = () => {
-  const uri = `mongodb+srv://${user}:${passwd}@${cluster}/${dbName}?retryWrites=true&w=majority`;
+export const dbConnect = (env?: string) => {
+  const finalEnv = env || process.env.NODE_ENV;
+  const finalDBName = finalEnv === 'test' ? dbName + '_Testing' : dbName;
+  const uri = `mongodb+srv://${user}:${passwd}@${cluster}/${finalDBName}?retryWrites=true&w=majority`;
+  debug(uri);
   return mongoose.connect(uri);
 };
